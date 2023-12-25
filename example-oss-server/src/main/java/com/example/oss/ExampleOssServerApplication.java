81c1eb9d7d3dc402fc88f9e5bd2644c336a3feca
Wangyw <admin@wangyw.cn>
2023-12-25 17:43:12 +0800

    - 初始化前后端分离项目

---
+++ example-oss-server/src/main/java/com/example/oss/ExampleOssServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOssServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleOssServerApplication.class, args);
+    }
+
+}
---
+++ example-oss-server/src/test/java/com/example/oss/ExampleOssServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleOssServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
