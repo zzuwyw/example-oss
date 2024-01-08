3c5566ec854043821d44b663cade54fcc7fbd92f
Wangyw <admin@wangyw.cn>
2024-01-08 09:23:34 +0800

    - 修改脚本

--- example-oss-server/src/main/resources/sqls/t_user.sql
+++ example-oss-server/src/main/resources/sqls/t_user.sql
@@ -19,16 +19,18 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
-   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录账号',
-   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录密码',
-   `enabled` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否可用',
-   `register_at` datetime NULL DEFAULT NULL COMMENT '注册时间',
-   PRIMARY KEY (`id`) USING BTREE,
-   UNIQUE INDEX `rds_idx_1`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `username` varchar(36) NOT NULL DEFAULT '' COMMENT '登录账号',
+  `password` varchar(128) NOT NULL DEFAULT '' COMMENT '登录密码',
+  `real_name` varchar(36) NOT NULL DEFAULT '' COMMENT '真实姓名',
+  `enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否可用',
+  `register_at` datetime DEFAULT NULL COMMENT '注册时间',
+  `last_login` datetime DEFAULT NULL COMMENT '最后登录时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rds_idx_1` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (1, 'admin', '$2a$10$sxhhrBb1DY7GLjOFZzgY4ub.AIzAqmgTtjyJ2MsNGjqO0hHfFs4Z.', 1, '2023-12-22 10:36:01');
+INSERT INTO `t_user` VALUES (1, 'admin', '$2a$10$sxhhrBb1DY7GLjOFZzgY4ub.AIzAqmgTtjyJ2MsNGjqO0hHfFs4Z.', '超级管理员', 1, '2023-12-22 10:36:01', null);
